4033ae2a53ff39f42717e9178082a7eb3c990b72
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-05-25 07:52:28 +0200

    Updated

---
+++ Client/build/precache-manifest.8aad9a8c2e454dd46038d11ee5a26fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfba1bf0344ce98e524f69f35ad97801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ebf6ac8afe3cedf8c7e",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "92db12be15a4cc373dc6",
+    "url": "/static/js/2.ccb292d4.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.ccb292d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebf6ac8afe3cedf8c7e",
+    "url": "/static/js/main.dcc5b536.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
